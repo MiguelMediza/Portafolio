64a9b9c0f7f47558a37fad78d2d1ee77d2a7d39c
Miguel Mediza <migueamediza@gmail.com>
2023-01-01 11:17:28 -0300

    Portafolio
    Portafolio Miguel Mediza

---
+++ script.js
@@ -0,0 +1,44 @@
+ $(document).ready(function () {
+  
+    //init scrolling parallax
+    $(window).scroll(function(e){
+        var scrolled = $(window).scrollTop();
+        if(scrolled < 750){
+            parallax()
+        }
+    });
+  
+    //define parallax function
+    function parallax(){
+      var scrolled = $(window).scrollTop();
+      $('#parallax').css('background-positionY',(scrolled * -0.5)+'px');
+    };
+    
+  }); 
+
+
+
+  jQuery('document').ready(function($){
+    
+    var menuBtn = $('.menu-icon'); 
+     menu = $('.navigation ul');
+     
+     menuBtn.click(function(){
+       
+         if(menu.hasClass('show')){
+          
+             menu.removeClass('show');   
+             
+         }
+         else{
+                  menu.addClass('show');   
+         }
+ 
+     
+   });             
+                   
+                   
+ });
+
+
+ 
