6f2acba848a05900f98bff66a99c1d9270c92013
Miguel Mediza <migueamediza@gmail.com>
2023-01-22 19:08:07 -0300

    Nuevos cambios con section contacto

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
- $(document).ready(function () {
-  
-    //init scrolling parallax
-    $(window).scroll(function(e){
-        var scrolled = $(window).scrollTop();
-        if(scrolled < 750){
-            parallax()
-        }
-    });
-  
-    //define parallax function
-    function parallax(){
-      var scrolled = $(window).scrollTop();
-      $('#parallax').css('background-positionY',(scrolled * -0.2)+'px');
-    };
-    
-  }); 
-
-
-
+//Funcion para activar y desactivar el icono del menú
   jQuery('document').ready(function($){
     
     var menuBtn = $('.menu-icon'); 
